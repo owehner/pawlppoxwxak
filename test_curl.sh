7292d4be12c1369f81fba10579c3d1e64aa7336f
Shubham <bbrooks870@student.msjc.edu>
2026-09-08 19:56:36 +0530

    test: test curl on runner

---
+++ test_curl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -I -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" "https://new3.moviesdrive.christmas/bigg-boss-season-20-2026/"
